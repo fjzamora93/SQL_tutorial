49c4cb723d0ba5b995c0f77eeef340ecef4d048f
fjzamora93 <148455736+fjzamora93@users.noreply.github.com>
2024-02-27 09:29:00 +0100

    update

--- ag2_proyecto/query1.sql
+++ ag2_proyecto/query1.sql
@@ -2,11 +2,13 @@ CREATE DATABASE IF NOT EXISTS ag2_proyectos;
 
 use ag2_proyectos;
 
-DELETE FROM empleados;
-DELETE FROM proyectos;
-DELETE from departamentos;
-DELETE FROM asignacion;
+DROP TABLE asignacion;
+DROP TABLE empleados;
+DROP TABLE departamentos;
+DROP TABLE proyectos;
 
+/*DESACTIVAMOS LAS RESTRICCIONES DE INTEGRIDAD REFERENCIAL */
+SET foreign_key_checks = 0;
 
 
 CREATE TABLE IF NOT EXISTS empleados(
@@ -15,19 +17,10 @@ CREATE TABLE IF NOT EXISTS empleados(
     apellidos VARCHAR(25) NOT NULL,
     sexo CHAR(1) DEFAULT " ",
     municipio VARCHAR(50)  DEFAULT " ",
-    fecha_nac DATE  DEFAULT " ",
+    fecha_nac DATE,
     sueldo INT(11) NOT NULL,
     idpto INT(11) NOT NULL
-    
 );
-
-INSERT IGNORE INTO empleados(idempleado, nombre, apellidos, sexo, fecha_nac, sueldo, municipio, idpto)
-	VALUES(10478, 'Alberto', 'Pérez López', 'M', '1965-09-12', 1500, 'Madrid', 1),
-		(10479, 'Gloria', 'Ruiz Ruiz', 'F', '1968-06-12', 1650, 'Sevilla', 2),
-		(10480, 'Antonio', 'García Montero', 'M', '1969-10-12', 1350, 'Madrid', 1),
-		(10481, 'Ana', 'López Ramírez', 'F', '1970-05-12', 1250, 'Sevilla', 3),
-		(10482, 'Eduardo', 'Chicón Terrales', 'M', '1920-05-12', 1470, 'Córdoba', 2);
-
 
 CREATE TABLE IF NOT EXISTS proyectos(
 idproyecto INT(11) NOT NULL,
@@ -36,37 +29,71 @@ fecha_ini DATE,
 fecha_fin DATE
 );
 
-INSERT IGNORE INTO proyectos (idproyecto, nombre, fecha_ini, fecha_fin)
-	VALUES(1, 'SINUBE', '2018-09-12', '2019-09-12'),
-		(2, 'TRASPI', '2017-09-12', '2019-09-12'),
-		(3, 'RUNTA', '2016-09-12', '2019-09-12'),
-		(4, 'CARTAL', '2019-05-12', '2019-09-12');
-
-
 CREATE TABLE IF NOT EXISTS departamentos(
 idpto INT(11) NOT NULL,
 nombre VARCHAR(25),
 presupuestos INT(11)
 );
 
-INSERT IGNORE INTO departamentos (idpto, nombre, presupuestos)
-	VALUES(1, 'I+D', 50000),
-		(2, 'Diseño', 25000),
-		(3, 'Ventas', 15000),
-		(4, 'Marketing', 10000);
-
-
 CREATE TABLE IF NOT EXISTS asignacion(
 idempleado INT(11) NOT NULL,
 idproyecto INT(11) NOT NULL
 );
 
-INSERT IGNORE INTO asignacion (idempleado,idproyecto)
+ALTER TABLE departamentos
+	ADD PRIMARY KEY (idpto);
+
+ALTER TABLE proyectos
+	ADD PRIMARY KEY (idproyecto);
+    
+ALTER TABLE empleados
+	ADD PRIMARY KEY (idempleado, nombre);
+
+ALTER TABLE empleados
+	ADD CONSTRAINT idpto_fk  FOREIGN KEY (idpto) 
+    REFERENCES departamentos(idpto) 
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE;
+    
+ALTER TABLE asignacion
+	ADD CONSTRAINT idempleado_fk FOREIGN KEY (idempleado) REFERENCES empleados(idempleado)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    ADD CONSTRAINT idproyecto_fk FOREIGN KEY (idproyecto) REFERENCES proyectos(idproyecto)
+		ON DELETE NO ACTION
+        ON UPDATE CASCADE;
+
+
+INSERT INTO empleados(idempleado, nombre, apellidos, sexo, fecha_nac, sueldo, municipio, idpto)
+	VALUES(10478, 'Alberto', 'Pérez López', 'M', '1965-09-12', 1500, 'Madrid', 1),
+		(10479, 'Gloria', 'Ruiz Ruiz', 'F', '1968-06-12', 1650, 'Sevilla', 2),
+		(10480, 'Antonio', 'García Montero', 'M', '1969-10-12', 1350, 'Madrid', 1),
+		(10481, 'Ana', 'López Ramírez', 'F', '1970-05-12', 1250, 'Sevilla', 3),
+		(10482, 'Eduardo', 'Chicón Terrales', 'M', '1920-05-12', 1470, 'Córdoba', 2);
+        
+INSERT INTO proyectos (idproyecto, nombre, fecha_ini, fecha_fin)
+	VALUES(1, 'SINUBE', '2018-09-12', '2019-09-12'),
+		(2, 'TRASPI', '2017-09-12', '2019-09-12'),
+		(3, 'RUNTA', '2016-09-12', '2019-09-12'),
+		(4, 'CARTAL', '2019-05-12', '2019-09-12');
+        
+INSERT INTO departamentos (idpto, nombre, presupuestos)
+	VALUES(1, 'I+D', 50000),
+		(2, 'Diseño', 25000),
+		(3, 'Ventas', 15000),
+		(4, 'Marketing', 10000);
+        
+INSERT INTO asignacion (idempleado,idproyecto)
 VALUES (10478, 1),
 	(10480, 3),
 	(10481, 1),
 	(10482, 2);
-    
+
+
+/*REESTABLECEMOS LAS RESTRICCIONES DE INTEGRIDAD REFERENCIAL */
+SET foreign_key_checks = 1;
+COMMIT;
+
     /*______________________CONSULTA 1______________________*/
 /*
 SELECT * FROM empleados;
@@ -74,6 +101,10 @@ SELECT * FROM proyectos;
 SELECT * From departamentos;
 SELECT * FROM asignacion;
 */
+
+
+
+
 
 /*______________________CONSULTA 2______________________*/
 SELECT nombre, fecha_ini AS "Fecha de inicio del proyecto" FROM proyectos
@@ -91,8 +122,10 @@ WHERE idproyecto IN (SELECT idproyecto FROM asignacion
             
 /*______________________CONSULTA 6______________________*/
 
-SELECT * FROM empleados 
-	WHERE municipio LIKE "Madrid" || municipio LIKE "Córdoba";
+SELECT * FROM empleados
+	WHERE municipio IN (SELECT municipio FROM empleados WHERE municipio LIKE "Madrid") ||
+    municipio IN (SELECT municipio FROM empleados WHERE municipio LIKE "Cordoba");
+
 
 SELECT * FROM empleados 
 	WHERE municipio LIKE "Madrid" OR municipio LIKE "Cordoba";
@@ -155,6 +188,7 @@ idpto IN (SELECT idpto FROM departamentos WHERE nombre LIKE "Diseño");
  SET sueldo =  sueldo + (sueldo/100*5)
  WHERE sueldo < 1400 AND
  idpto IN (SELECT idpto FROM departamentos WHERE nombre LIKE "I+D");
- 
- SELECT * FROM empleados;
-    
+
+
+
+
